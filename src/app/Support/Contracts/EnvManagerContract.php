bbd47f843d9ad8fdd136fd9dfc07079fde358f03
Рустам <rusiq@list.ru>
2019-11-03 21:53:58 +1000

    Определение основного функционала

--- src/app/Support/Contracts/EnvManagerContract.php
+++ src/app/Support/Contracts/EnvManagerContract.php
@@ -4,5 +4,68 @@ namespace Russsiq\EnvManager\Support\Contracts;
 
 interface EnvManagerContract
 {
-    // 
+    /**
+     * Получить полный путь к файлу окружения.
+     *
+     * @return string
+     */
+    public function filePath(): string;
+
+    /**
+     * Проверить физическое существование файла окружения.
+     *
+     * @return bool
+     */
+    public function fileExists(): bool;
+
+    /**
+     * Проверить существование значения для указанной переменной окружения.
+     *
+     * @param string $name Имя переменной.
+     *
+     * @return bool
+     */
+    public function has(string $name): bool;
+
+    /**
+     * Получить значения для указанной переменной окружения.
+     *
+     * @param string $name Имя переменной.
+     *
+     * @return string|null
+     */
+    public function get(string $name): string;
+
+    /**
+     * Установить значение для переменной окружения.
+     *
+     * @param string      $name  Имя переменной.
+     * @param string|null $value Значение переменной.
+     *
+     * @return self
+     */
+    public function set(string $name, $value = null): self;
+
+    /**
+     * Установить значения для переменных окружения.
+     *
+     * @param array $data  Массив из имен и значений.
+     *
+     * @return self
+     */
+    public function setMany(array $data): self;
+
+    /**
+     * Сохранить файл окружения.
+     *
+     * @return mixed
+     */
+    public function save(): bool;
+
+    /**
+     * Создать файл окружения.
+     *
+     * @return mixed
+     */
+    public function create(): bool;
 }
