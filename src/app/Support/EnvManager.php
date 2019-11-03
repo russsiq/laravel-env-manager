06ac0ed736573d8d14babe31e65d91b8c737f98b
Рустам <rusiq@list.ru>
2019-11-03 17:00:00 +1000

    Внедрение зависимости Application в менеджер

--- src/app/Support/EnvManager.php
+++ src/app/Support/EnvManager.php
@@ -2,12 +2,26 @@
 
 namespace Russsiq\EnvManager\Support;
 
+use Illuminate\Foundation\Application;
+
 use Russsiq\EnvManager\Support\Contracts\EnvManagerContract;
 
 class EnvManager implements EnvManagerContract
 {
-	public function __construct()
+    /**
+     * Экземпляр приложения.
+     *
+     * @var Application
+     */
+    protected $app;
+
+    /**
+     * Создать новый экземпляр менеджера файла переменных окружения.
+     * 
+     * @param  Application  $app
+     */
+	public function __construct(Application $app)
 	{
-		//
+		$this->app = $app;
 	}
 }
