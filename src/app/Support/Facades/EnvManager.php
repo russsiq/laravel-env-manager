9554c1679c5943123429ce297fc7adf5404453f5
Рустам <rusiq@list.ru>
2019-11-24 11:36:29 +1000

    Добавлены ссылки на методы в комментариях фасаде

--- src/app/Support/Facades/EnvManager.php
+++ src/app/Support/Facades/EnvManager.php
@@ -4,6 +4,10 @@ namespace Russsiq\EnvManager\Support\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @see \Russsiq\EnvManager\Support\Contracts\EnvManagerContract
+ * @see \Russsiq\EnvManager\Support\EnvManager
+ */
 class EnvManager extends Facade
 {
     protected static function getFacadeAccessor()
