7e494d750af54e3b2f4f8717e9db3b2290c950a9
Рустам <rusiq@list.ru>
2020-05-29 07:50:24 +1000

    [Тесты] Добавлен тестовый класс `EnvManagerTest`

---
+++ tests/Feature/Support/EnvManagerTest.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Feature\Support;
+
+// Тестируемый класс.
+use Russsiq\EnvManager\Support\EnvManager;
+
+// Сторонние зависимости.
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass \Russsiq\EnvManager\Support\EnvManager
+ */
+class EnvManagerTest extends TestCase
+{
+    public function testExample(): void
+    {
+        $this->assertTrue(true);
+    }
+}
