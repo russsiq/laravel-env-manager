3e76c2a077ee9594a24148bd015bb4b6565a12ea
Рустам <rusiq@list.ru>
2020-05-29 11:25:20 +1000

    Update EnvManagerTest.php
    Определены зависимости

--- tests/Feature/Support/EnvManagerTest.php
+++ tests/Feature/Support/EnvManagerTest.php
@@ -5,8 +5,12 @@ namespace Tests\Feature\Support;
 // Тестируемый класс.
 use Russsiq\EnvManager\Support\EnvManager;
 
+// Базовые расширения PHP.
+
 // Сторонние зависимости.
+use Illuminate\Encryption\Encrypter;
 use PHPUnit\Framework\TestCase;
+use Russsiq\EnvManager\Contracts\EnvManagerContract;
 
 /**
  * @coversDefaultClass \Russsiq\EnvManager\Support\EnvManager
