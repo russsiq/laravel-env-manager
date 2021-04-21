c090abc8bc4329a3d1e023785ca24d4cbef811c5
russsiq <rusiq@list.ru>
2021-04-21 15:22:10 +1000

    Update EnvManagerServiceProvider.php

--- src/EnvManagerServiceProvider.php
+++ src/EnvManagerServiceProvider.php
@@ -2,19 +2,20 @@
 
 namespace Russsiq\EnvManager;
 
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 use Russsiq\EnvManager\Support\EnvManager;
 
 class EnvManagerServiceProvider extends ServiceProvider
 {
     /**
-     * Регистрация Менеджера файла переменных окружения.
+     * Register services.
      *
      * @return void
      */
     public function register()
     {
-        $this->app->singleton('env-manager', function ($app) {
+        $this->app->singleton('env-manager', function (Application $app) {
             return new EnvManager(
                 $app->environmentFilePath(),
                 $app->config->get('app.cipher')
